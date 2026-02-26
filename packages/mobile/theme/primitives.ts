6a12cdde108a4a5c314ab7339122877f9899caea
Jarius Hayes <sayrilamar@gmail.com>
2026-02-26 14:06:57 -0500

    feat(mobile): scaffold initial React Native UI

---
+++ packages/mobile/theme/primitives.ts
@@ -0,0 +1,6 @@
+import { createBox, createText } from '@shopify/restyle';
+
+import type { Theme } from './theme';
+
+export const Box = createBox<Theme>();
+export const Text = createText<Theme>();
